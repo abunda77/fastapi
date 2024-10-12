9a31d94d129f9c673bf8b5384f223d1fa3d924b5
Erie Putranto <erieputranto@gmail.com>
2024-10-12 22:31:29 +0700

    update: add pool size

--- app/db/database.py
+++ app/db/database.py
@@ -14,8 +14,8 @@ logger = logging.getLogger(__name__)
 # Buat engine dengan connection pooling
 engine = create_engine(settings.
     DATABASE_URL,
-    pool_size=5,
-    max_overflow=10,
+    pool_size=100,
+    max_overflow=200,
     pool_timeout=30,
     pool_recycle=1800,
     pool_pre_ping=True,
